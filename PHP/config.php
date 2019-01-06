cf952070734dd0d013437c7cf66e200fb21d36b1
LenssenAlexander <Alexander.LenssenMG@gmail.com>
2019-01-06 17:41:45 +0100

    Config

--- PHP/config.php
+++ PHP/config.php
@@ -1,8 +1,8 @@
 <?php
     return array (
         'host' => 'localhost',
-        'username' => 'db_iiigel_user',
-        'password' => 'qi6g3*0L',
-        'database' => 'DB_iiigel'
+        'username' => 'root',
+        'password' => '',
+        'database' => 'iiigel'
     );
 ?>
