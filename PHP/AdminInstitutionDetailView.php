d304308e52f13e1a5b1e934f547c94d80f902337
JonathanNiehaus <45129945+JonathanNiehaus@users.noreply.github.com>
2019-02-03 19:02:00 +0100

    Update AdminInstitutionDetailView.php

--- PHP/AdminInstitutionDetailView.php
+++ PHP/AdminInstitutionDetailView.php
@@ -11,7 +11,7 @@
     $myPage = str_replace($search,$replace,$myRow);
   
 
-	if((!$ODB->isAdmin($_SESSION['user']))and(!$ODB->isInstitutionsLeader($_SESSION['user']))) {//AL Nur Admins und Institutionsleiter dürfen die Seite sehen
+	if((!$ODB->isAdmin($_SESSION['user']))and(!$ODB->isInstitutionsLeader($_SESSION['user']))) {
 		 echo "Sie haben nicht die benötigte Berechtigung um diese Seite anzusehen.";
         exit;
     } else {
@@ -19,7 +19,7 @@
 	$myPage = str_replace('%Navigation%',getNavigation(),$myPage);
      $toAdd = "";
     $myUsers = $ODB->getUsersFromInstitution($_GET['InstitutionsID']);
-    for ($i=0; $i< sizeof($myUsers);$i++){   //AL Gibt alle Nutzer einer Institution aus
+    for ($i=0; $i< sizeof($myUsers);$i++){   
         $myRow = file_get_contents('../HTML/InstitutionDetailViewTablerow.html');
         $search = array('%Vorname%','%Nachname%','%Username%','%Email%');
         $replace = array($myUsers[$i] ->getsFirstName(),$myUsers[$i]->getsLastName(),$myUsers[$i]->getsUsername(),$myUsers[$i]->getsEMail());
@@ -31,10 +31,10 @@
        
     }
     
-    $add = '';
+    $add = ''; //JN -Liste aller user die nicht in der institution sind (Benutzer hinzufügen tabele)
     $myUsers= $ODB->GetAllUsersNotInInstitution($_GET['InstitutionsID']);
 
-	for ($a=0;$a<sizeof($myUsers);$a++){//AL Macht eine Liste der Leute, welche nicht in der Institution sind, falls man sie hinzufügen will.
+	for ($a=0;$a<sizeof($myUsers);$a++){
 		$myRow = file_get_contents('../HTML/AdminGroupUserListitem.html');
       
         $search = array('%Vorname%','%Nachname%','%UserID%');
@@ -44,9 +44,9 @@
        
 	}
         
-    $linksAdded ='';
+    $linksAdded ='';//Liste aller user in der institution
     $links= $ODB->getAllLinksFromInstitution(intval($_GET['InstitutionsID']));
-	for ($a=0;$a<sizeof($links);$a++){//AL Gibt alle Einladungslinks in die Institution aus.
+	for ($a=0;$a<sizeof($links);$a++){
 		$myRow = file_get_contents('../HTML/AdminInstitutionLinkItem.html');
       
         $search = array('%LinkString%','%startDate%','%endDate%');
@@ -56,7 +56,7 @@
        
 	}
         
-    if (isset($_POST['addLink'])){//AL Wenn man den Button für das erstellen eines Buttons gedrückt hat
+    if (isset($_POST['addLink'])){
         $ODB->addInstitutionInvitationLink($_POST['link'],$_GET['InstitutionsID'],$_POST['startdate'],$_POST['enddate']);
         header("Location: ../PHP/AdminInstitutionDetailView.php?InstitutionsID=".$_GET['InstitutionsID']);
         exit;
